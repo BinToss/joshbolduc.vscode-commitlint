304f99c53027817b0ae312070f1e62e00dc90adb
Noah Sherwin <noahrsherwin@gmail.com>
2025-03-06 05:44:32 -0800

    style: format eslint.config.mjs w/ prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { default as js } from '@eslint/js';
 import prettier from 'eslint-config-prettier';
-import { default as importX } from 'eslint-plugin-import-x'
-import globals from 'globals'
+import { default as importX } from 'eslint-plugin-import-x';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 // re: migration to ESLint v9; See https://eslint.org/docs/latest/extend/plugin-migration-flat-config
@@ -19,29 +19,33 @@ export default tseslint.config(
       globals: { ...globals.node },
       parserOptions: {
         // replaces `project: boolean | string`; See https://typescript-eslint.io/blog/announcing-typescript-eslint-v8-beta/#project-service
-        projectService: true
+        projectService: true,
       },
     },
     linterOptions: {
-      reportUnusedDisableDirectives: true
+      reportUnusedDisableDirectives: true,
     },
-    ignores: ["src/git.d.ts", "node_modules/**/*", "dist/**/*", "out/**/*"],
+    ignores: [
+      'node_modules/**/*',
+      'dist/**/*',
+      'out/**/*',
+    ],
     rules: {
-      curly: ["error", "all"],
-      "import-x/order": ["warn", { "alphabetize": { "order": "asc" } }],
-      "object-shorthand": [
-        "warn",
-        "always",
+      curly: ['error', 'all'],
+      'import-x/order': ['warn', { alphabetize: { order: 'asc' } }],
+      'object-shorthand': [
+        'warn',
+        'always',
         {
-          "ignoreConstructors": false,
-          "avoidQuotes": true
-        }
+          ignoreConstructors: false,
+          avoidQuotes: true,
+        },
       ],
-      "sort-imports": [
-        "warn",
-        { "ignoreCase": true, "ignoreDeclarationSort": true }
-      ]
-    }
+      'sort-imports': [
+        'warn',
+        { ignoreCase: true, ignoreDeclarationSort: true },
+      ],
+    },
   },
   {
     // See https://typescript-eslint.io/troubleshooting/typed-linting/performance/#eslint-plugin-import,
@@ -51,7 +55,7 @@ export default tseslint.config(
       'import-x/namespace': 'off',
       'import-x/default': 'off',
       'import-x/no-named-as-default-member': 'off',
-      'import-x/no-unresolved': 'off'
-    }
-  }
-)
+      'import-x/no-unresolved': 'off',
+    },
+  },
+);
